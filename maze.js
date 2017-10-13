ee108291993b58761ffcb5521fc8fd7aa35b6f5f
AundreE <32345683+AundreE@users.noreply.github.com>
2017-10-13 18:44:52 -0500

    Excercise 4

--- maze.js
+++ maze.js
@@ -1,12 +1,21 @@
 var loser = false;  // whether the user has hit a wall
 
 window.onload = function() {    
-   document.getElementById("end").onmouseover = overEnd;
+    document.getElementById("start").onclick = startingClick;
+    document.getElementById("end").onmouseover = overEnd;
     var boundaries = document.querySelectorAll("div#maze div.boundary");
     for (var i = 0; i < boundaries.length; i++) {
         boundaries[i].onmouseover = overBoundary;
     }
 };
+function startingClick(){
+	loser = false;
+	var boundaries = document.querySelectorAll("div#maze div.boundary");
+	for (var x = 0; x < boundaries.length; x++){
+		boundaries[x].classList.remove("youlose"); 
+	}
+}
+
 
 function overBoundary() {
     loser = true;
