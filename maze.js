9a9744bac40e8c96585a34d1cbaaac05dadf9ba4
AundreE <32345683+AundreE@users.noreply.github.com>
2017-10-20 15:49:52 -0500

    Excercise 6

--- maze.js
+++ maze.js
@@ -1,35 +1,33 @@
 var loser = false;  // whether the user has hit a wall
 
-window.onload = function() {    
+window.onload = function() {
     document.getElementById("start").onclick = startingClick;
     document.getElementById("end").onmouseover = overEnd;
-    var boundaries = document.querySelectorAll("div#maze div.boundary");
+    document.getElementById("maze").onmouseleave=overBoundary;
+    var boundaries = document.querySelectorAll("div#maze div.boundary ");
     for (var i = 0; i < boundaries.length; i++) {
         boundaries[i].onmouseover = overBoundary;
     }
 };
 function startingClick(){
 	loser = false;
-	var boundaries = document.querySelectorAll("div#maze div.boundary");
+    document.getElementById("status").innerHTML ="Move your mouse over the S to begin";
+	var boundaries = document.querySelectorAll("div#maze div.boundary ");
 	for (var x = 0; x < boundaries.length; x++){
 		boundaries[x].classList.remove("youlose"); 
 	}
 }
 
-
 function overBoundary() {
     loser = true;
-    var boundaries = document.querySelectorAll("div#maze div.boundary");
+    document.getElementById("status").innerHTML ="You lose!";
+    var boundaries = document.querySelectorAll("div#maze div.boundary ");
     for (var i = 0; i < boundaries.length; i++) {
         boundaries[i].classList.add("youlose");
     }
 }
-
-
 function overEnd() {
-    if(loser) {
-        document.getElementById("status").innerHTML ="Nice try but u were not successful :(";
-    } else {
+    if(!loser) {
         document.getElementById("status").innerHTML ="You win!";
     }
 }
